c831d28eed2624cfad5d5d51f475a41215d56be9
Ivan Bikeev <dead43rus@gmail.com>
2021-03-13 20:41:37 +0300

    tenth v

--- Bot/app.py
+++ Bot/app.py
@@ -12,12 +12,12 @@ token = token
 Bot = Bot(token)
 db = DataBase()
 target = Target()
+target_name = 'tail43'
+boss = 'L_tail'
 
 @app.route('/{}'.format(token),methods=["POST"])
 def make_resp():
     result = request.json
-    target_name = 'tail43'
-    boss = 'L_tail'
     print(result)
     callback = result.get('callback_query')
     if callback:
